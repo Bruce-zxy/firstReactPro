14f529c2be3da3df66bc703d037470bf7e5be143
bruce-zxy <bruce_zxy@163.com>
2017-04-26 09:41:10 +0800

    第二次项目改动

--- src/entry.js
+++ src/entry.js
@@ -1,23 +1,14 @@
 
-'use strict';
+/**
+ *
+ * @authors jarson_7426@163.com
+ * @date    2016-10-21 16:42:35
+ */
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Router, Route, Link, IndexRoute, Redirect, hashHistory } from 'react-router';
-import { createHistory, createHashHistory, useBasename } from 'history';
+'use strict';
 
 //引入样式文件
 import './styles/app.scss';
 
-import App from './components/App.js'
-import List from './components/List.js'
-import Detail from './components/Detail.js'
-
-//最终渲染
-ReactDom.render((
-    <Router history={hashHistory}>
-        <Route path='/' component={App}></Route>
-	        <Route path='/list' component={List} />
-	        <Route path='/detail' component={Detail} />
-    </Router>
-), document.getElementById('app'));
+//引入组件
+import './components/app.js';
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@
 'use strict';
 
 var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
 module.exports = {
     devtool: 'eval-source-map',
 
-    entry: __dirname + '/src/entry.js', //唯一入口文件
+    entry: {
+        main: './src/entry.js', //唯一入口文件
+        vendor: ['react']
+    },
     output: {
-        path: __dirname + '/build', //打包后的文件存放的地方
-        filename: 'bundle.js' //打包后输出文件的文件名
+        path: './build', //打包后的文件存放的地方
+        filename: 'main.js', //打包后输出文件的文件名
+        publicPath: 'http://localhost:8888/build/'  //启动本地服务后的根目录
     },
 
     module: {
@@ -17,8 +22,16 @@ module.exports = {
             { test: /\.js$/, loader: "jsx!babel", include: /src/},
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css!postcss")},
             { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass")},
-            { test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
+            { test: /\.(png|jpg|gif)$/, loader: 'url?limit=819200'}
         ]
+    },
+
+    babel: {
+        presets: ['es2015', 'stage-0', 'react'],
+        plugins: ['transform-runtime', ['import', {
+          libraryName: 'antd',
+          style: 'css'
+        }]]
     },
 
     postcss: [
@@ -35,6 +48,10 @@ module.exports = {
 
     plugins: [
         new ExtractTextPlugin('main.css'),
+        new CommonsChunkPlugin({
+           name: 'vendor',
+           filename: 'vendor.js'
+        })
     ]
 
 }
